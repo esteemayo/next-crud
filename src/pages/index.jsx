ea8cc8b50aafe1635c0dd8726d62b954222b3076
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-16 00:07:09 +0100

    changed directory

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World!!!</h1>
+    </div>
+  )
+}
+
+export default Home;
