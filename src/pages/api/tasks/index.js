7b6a384e43293d691f866caa5051dd015a26d7b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-16 00:37:02 +0100

    created get and post api requests for tasks

---
+++ src/pages/api/tasks/index.js
@@ -0,0 +1,37 @@
+import Morgan from 'morgan';
+
+import Task from '@/models/Task';
+import { dbConnect } from '@/utils/mongo';
+import { runMiddleware } from '@/utils/runMiddleware';
+
+dbConnect();
+
+const handler = async (req, res) => {
+  const { method, body } = req;
+  const morgan = Morgan('dev');
+
+  switch (method) {
+    case 'GET':
+      try {
+        const tasks = await Task.find();
+        await runMiddleware(req, res, morgan);
+        return res.status(200).json(tasks);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    case 'POST':
+      try {
+        const task = await Task.create({ ...body });
+        await runMiddleware(req, res, morgan);
+        return res.status(201).json(task);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    default:
+      break;
+  }
+};
+
+export default handler;
