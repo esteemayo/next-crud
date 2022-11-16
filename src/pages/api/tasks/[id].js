030563d16ae79fcfd1e339ff3b090f5ca768ee19
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-16 01:08:09 +0100

    created get, patch and delete api requests for tasks

---
+++ src/pages/api/tasks/[id].js
@@ -0,0 +1,65 @@
+import Morgan from 'morgan';
+
+import Task from '@/models/Task';
+import { dbConnect } from '@/utils/mongo';
+import { runMiddleware } from '@/utils/runMiddleware';
+
+dbConnect();
+
+const handler = async (req, res) => {
+  const { method, query: { id: taskId }, body } = req;
+  const morgan = Morgan('dev');
+
+  switch (method) {
+    case 'GET':
+      try {
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+          return res.status(404).json({ message: `No task found with the given ID → ${taskId}` });
+        }
+
+        await runMiddleware(req, res, morgan);
+        return res.status(200).json(task);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    case 'PATCH':
+      try {
+        const task = await Task.findByIdAndUpdate(taskId, { $set: { ...body } }, {
+          new: true,
+          runValidators: true,
+        });
+
+        if (!task) {
+          return res.status(404).json({ message: `No task found with the given ID → ${taskId}` });
+        }
+
+        await runMiddleware(req, res, morgan);
+        return res.status(200).json(task);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    case 'DELETE':
+      try {
+        const task = await Task.findByIdAndDelete(taskId);
+
+        if (!task) {
+          return res.status(404).json({ message: `No task found with the given ID → ${taskId}` });
+        }
+
+        await runMiddleware(req, res, morgan);
+        return res.status(204).json();
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    default:
+      res.setHeader('Allow', ['GET', 'PATCH', 'DELETE']);
+      return res.status(405).json({ message: `This method (${method}) is not supported` });
+  }
+};
+
+export default handler;
