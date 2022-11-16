8856e11c4b553f7ed6ece2764e78601132ae2d30
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-16 02:07:46 +0100

    created meta for seo

---
+++ src/components/Meta.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='keywords' content={keywords} />
+      <meta name='description' content={description} />
+      <meta http-equiv='X-UA-Compatible' content='IE=7' />
+      <meta http-equiv='Content-Type' content='text/html;charset=UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Task App',
+  keywords: '',
+  description: '',
+};
+
+export default Meta;
