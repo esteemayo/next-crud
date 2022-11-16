13415a96370ea9969557af4be7a0d0140785dc97
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-16 02:51:13 +0100

    added new env variables

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_DEV_URL: process.env.NEXT_PUBLIC_API_DEV_URL,
+    NEXT_PUBLIC_API_PROD_URL: process.env.NEXT_PUBLIC_API_PROD_URL
+  }
 }
 
 module.exports = nextConfig
