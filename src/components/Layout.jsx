debab2adfe32963748dc5b5162c49ace8d0cbd5e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-17 01:49:48 +0100

    include react toastify

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
-import Meta from './Meta'
-import Navbar from './Navbar'
+import { ToastContainer } from 'react-toastify';
+
+import Meta from './Meta';
+import Navbar from './Navbar';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Meta />
       <Navbar />
+      <ToastContainer />
       {children}
     </div>
   );
